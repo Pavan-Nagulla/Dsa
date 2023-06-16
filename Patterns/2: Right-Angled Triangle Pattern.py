2869a7e956e7a7edaa31dec7eab6d807415bd4be
pavannagulla <pavankalyannagulla28@gmail.com>
2023-06-16 22:29:32 +0530

    pattern 2 added

---
+++ Patterns/2: Right-Angled Triangle Pattern.py
@@ -0,0 +1,19 @@
+"""
+Input Format: N = 6
+Result:
+* 
+* * 
+* * *
+* * * *
+* * * * *
+* * * * * *
+
+"""
+
+n=int(input())
+
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
