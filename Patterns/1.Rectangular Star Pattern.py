4689630495049503b9b998c872fcbbb4bff5c231
pavannagulla <pavankalyannagulla28@gmail.com>
2023-06-16 22:17:50 +0530

    pattern folder added

---
+++ Patterns/1.Rectangular Star Pattern.py
@@ -0,0 +1,18 @@
+"""
+Input: N = 6
+Output:
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+
+"""
+
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print("")
